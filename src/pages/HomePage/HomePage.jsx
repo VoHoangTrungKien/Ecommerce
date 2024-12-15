939cc9806ebad7c46fe1434fadab84d95ee453fb
Kien <vokien.201102@gmail.com>
2024-12-16 02:46:05 +0700

    update-banner

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,9 +6,9 @@ import {
   WrapperProducts,
   WrapperTypeProduct,
 } from "./style";
-import slider1 from "../../assets/images/slider1.webp";
-import slider2 from "../../assets/images/slider2.webp";
-import slider3 from "../../assets/images/slider3.webp";
+import slider6 from "../../assets/images/slider6.webp";
+import slider7 from "../../assets/images/slider7.webp";
+import slider8 from "../../assets/images/slider8.webp";
 import CardComponent from "../../components/CardComponent/CardComponent";
 import { useQuery } from "@tanstack/react-query";
 import * as ProductService from "../../services/ProductService";
@@ -72,7 +72,7 @@ const HomePage = () => {
           id="container"
           style={{ height: "100%", width: "1270px", margin: "0 auto" }}
         >
-          <SliderComponent arrImages={[slider1, slider2, slider3]} />
+          <SliderComponent arrImages={[slider6, slider8, slider8]} />
           <WrapperProducts>
             {products?.data?.map((product) => {
               return (
