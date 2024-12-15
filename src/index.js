15460d8c2ce1e4ef7258b567b3c3f759d1af2192
Kien <vokien.201102@gmail.com>
2024-12-16 02:25:41 +0700

    chatbox

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import { Provider } from 'react-redux'
-import { persistor, store } from './redux/store'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PersistGate } from "redux-persist/integration/react";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const queryClient = new QueryClient()
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
 root.render(
-  // <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  // </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Thêm đoạn mã TaggoAI ở đây
+<script
+  async
+  data-taggo-botid="675f18b6cb02892baf382a70"
+  src="https://widget.taggo.chat/v2.js"
+></script>;
+
 reportWebVitals();
